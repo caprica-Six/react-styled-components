46cdef5c5256f6cc008dec34f4b73a313686a890
caprica-Six <mogwais@outlook.com>
2019-01-10 13:43:25 +0000

    add more components + start GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-// import Button from './components/common/Button';  
-import { ThemeProvider } from 'styled-components';
+import Button from './components/common/Button';  
+import styled, { ThemeProvider } from 'styled-components';
 import LightTheme from "./theme/light";
 import DarkTheme from "./theme/dark";
-// import PasswordInput from './components/common/PasswordInput';
-import { Button } from 'reactstrap';
+import PasswordInput from './components/common/PasswordInput';
+import { Button as ReactstrapButton } from 'reactstrap';
 import PrimaryButton from "./components/common/PrimaryButton";
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+
+  }
+`
+const ButtonWrapper = styled.div`
+  padding: 16px 0;
+`
+
+const AppWrapper = styled.main`
+  display: flex;
+  max-width: 80%;
+  flex-direction: column;
+  align-items: center;
+  margin: 1em auto;
+  background: ${props => props.theme.backgroundColor}
+  color: ${props => props.theme.primaryColor}
+`
+
+// Overrride Reactstrap button, to avoid use of !important!!
+const ReactstrapRoundedButton = styled(ReactstrapButton)`
+  &.btn {
+    padding: 10px 25px;
+    border-radius: 100px;
+  }
+`
+const FacebookButton = styled(Button)`
+  border: none;
+  background: #3b5998;
+  color: white;
+`
 
 class App extends Component {
   state = { 
@@ -30,23 +63,36 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={this.state.theme}>
-        <Fragment>
-          
-          {/* <Button onClick={this.handleToggleTheme}> Ok </Button>
+        <AppWrapper>
+          <h1>React Styled Components</h1>
+          <Button onClick={this.handleToggleTheme}> Ok </Button>
           <section>
             <PasswordInput showPassword={this.state.showPassword} />
             <Button onClick={this.handleTogglePassword}>
               {this.state.showPassword ? 'Hide': 'Show'}
             </Button>
-          </section> */}
+          </section> 
 
 
           {/* ReactStrap buttons */}
-          <div>
-            <Button color="primary">primary</Button>{' '}
-            <PrimaryButton>custom button</PrimaryButton>
-          </div>
-        </Fragment>
+            <ButtonWrapper>
+              <Button color="primary">primary</Button>
+            </ButtonWrapper>
+            <ButtonWrapper>
+              <PrimaryButton>custom button</PrimaryButton>
+            </ButtonWrapper>
+            <ButtonWrapper>
+              <ReactstrapButton color="info">reactstrap button</ReactstrapButton>
+            </ButtonWrapper>
+            <ButtonWrapper>
+              <ReactstrapRoundedButton color="primary">
+                 existing component
+              </ReactstrapRoundedButton>
+            </ButtonWrapper>
+            <ButtonWrapper>
+              <FacebookButton>Facebook button</FacebookButton>
+            </ButtonWrapper>
+        </AppWrapper>
       </ThemeProvider>
     );
   }

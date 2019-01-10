747aa5c93cb6695c1ca9b28f5cdaad7eca2aa55e
caprica-Six <mogwais@outlook.com>
2019-01-10 10:53:50 +0000

    initial styled component

---
+++ src/components/common/Button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button  = styled.button`
+    background: white;
+    bordeR: 1px solid black;
+    padding: 10px;
+`;
+
+export default Button;
+
+
